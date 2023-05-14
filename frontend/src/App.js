d3f741bd86be94816e2c566be88115b4e9c79392
njiddasalifu <njiddasalifu@gmail.com>
2023-05-14 10:59:18 +0100

    working on header files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+    <Header />
+    </>
+  )
 }
 
 export default App;
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React from 'react';
+import './Header.css';
 
-const Header = ()=>{
-    return(
-        <div><Header></Header></div>
-    )
-}
+const Header = () => (
+    
+  <nav className='header'>
+    {/* <h2>Hi, Welcome to World Travels.</h2>
+     */}
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/tours">Tours</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/login">Login</a></li>
+    </ul>
+  </nav>
+);
+
 export default Header;
