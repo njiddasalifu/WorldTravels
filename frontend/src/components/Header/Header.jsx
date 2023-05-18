d5fac3c99de659dc823aec774f5f6197131e89f9
njiddasalifu <njiddasalifu@gmail.com>
2023-05-19 00:10:13 +0100

    working on the header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,10 +6,11 @@ const Header = () => (
   <nav className='header'>
     {/* <h2>Hi, Welcome to World Travels.</h2>
      */}
-    <ul>
+    <ul className='uls'>
       <li><a href="/">Home</a></li>
       <li><a href="/tours">Tours</a></li>
       <li><a href="/about">About</a></li>
+      <li><a href="./contact">Contact</a></li>
       <li><a href="/login">Login</a></li>
     </ul>
   </nav>
