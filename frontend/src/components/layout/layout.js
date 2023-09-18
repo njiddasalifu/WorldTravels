6d1dcc85440be710fdee3931f8b82b645aef77c4
njiddasalifu <njiddasalifu@gmail.com>
2023-09-18 14:57:40 +0100

    refactoting the layout

---
+++ frontend/src/components/layout/layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from './Header';
+import Footer from './Footer';
+
+const Layout = ()=>{
+    return(
+        <div>
+        <Header />
+        {/* put main pages here */}
+        <Footer />
+      </div>
+    )
+}
+export default Layout;
